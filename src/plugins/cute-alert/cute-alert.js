afdf432233a6c621ff3d5d39ed5d94ffb1389409
wulujunwlj <wulujunwlj@163.com>
2016-04-08 18:22:38 +0800

    add alert

---
+++ src/plugins/cute-alert/cute-alert.js
@@ -0,0 +1,12 @@
+/**
+ * add by wlj
+ * @ 20160408
+ */
+
+(function($) {
+	$.fn.cuteAlert = function(options) {
+		var opts = $.extend({}, $.fn.cuteAlert.defaults, options);
+	};
+
+	$.fn.cuteAlert.defaults = {};
+}(jQuery));
